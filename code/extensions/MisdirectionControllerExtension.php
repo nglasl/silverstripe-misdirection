1b6202e41a206cab13d3f40dc677194495e82a72
Jake Bentvelzen <jake@silverstripe.com.au>
2016-06-01 13:30:08 +1000

    fix(determineFallback): Fix bug where determineFallback was not working as expected

--- code/extensions/MisdirectionControllerExtension.php
+++ code/extensions/MisdirectionControllerExtension.php
@@ -5,8 +5,19 @@ class MisdirectionControllerExtension extends Extension {
 	public function onBeforeHTTPError($statusCode, $request) {
 		if ($statusCode == 404) 
 		{
+			// Get URL and remove the last URLSegment (ie. the current page)
+			$dirParts = explode('/', $request->getURL(true));
+			array_pop($dirParts);
+			$fallbackURL = implode('/', $dirParts);
+			// HACK(Jake): 'determineFallback' requires last URLSegment to not exist in SiteTree
+			//			   (as of 2016-06-01)
+			$fallbackURL .= '/ ';
+
+			/**
+			 * @var MisdirectionService
+			 */
 			$service = Injector::inst()->get('MisdirectionService');
-			$fallback = $service->determineFallback($request->getURL(true));
+			$fallback = $service->determineFallback($fallbackURL);
 			if ($fallback)
 			{
 				$response = $this->owner->getResponse();
